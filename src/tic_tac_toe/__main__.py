a96094ec581df408a26592127a7a01cd52fab1ff
Stanley5249 <seer852741@gmail.com>
2025-03-21 00:35:33 +0800

    refactor: improve import statements and formatting

--- src/tic_tac_toe/__main__.py
+++ src/tic_tac_toe/__main__.py
@@ -1,14 +1,13 @@
 import sys
 
-from PySide6 import QtWidgets
-import PySide6.QtGui as QtGui
+from PySide6 import QtGui, QtWidgets
 
 from .gui import MyWidget
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    app.setPalette(QtGui.QPalette(QtGui.QColor('#222')))
+    app.setPalette(QtGui.QPalette(QtGui.QColor("#222")))
     Form = MyWidget()
     Form.show()
     sys.exit(app.exec())
