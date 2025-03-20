b3ecce6bb000be2278901d3a5b9aeb1f0e032ae5
limitx0 <38377176+limitx0@users.noreply.github.com>
2025-03-21 00:04:23 +0800

    Dark Background in Light Mode

--- src/tic_tac_toe/__main__.py
+++ src/tic_tac_toe/__main__.py
@@ -1,12 +1,14 @@
 import sys
 
 from PySide6 import QtWidgets
+import PySide6.QtGui as QtGui
 
 from .gui import MyWidget
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
+    app.setPalette(QtGui.QPalette(QtGui.QColor('#222')))
     Form = MyWidget()
     Form.show()
     sys.exit(app.exec())
